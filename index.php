e167ba813ef02050fef21c3cd6c936f5ad11ad87
rga001 <rga001@uark.edu>
2014-04-07 17:42:17 -0500

    added top polls listing to main page; displays link if user session is set;

--- index.php
+++ index.php
@@ -34,4 +34,49 @@ echo <<<_END
 </body>
 </html>
 _END;
+
+	include_once('PollModel.php');
+	
+	$pollModel = new PollModel();
+	$topPolls = $pollModel->getTopPolls();
+	$count = 0;
+	echo <<<_END
+		<html><body>
+_END;
+	while ($row = mysql_fetch_array($topPolls)){
+		
+?>
+		<div style="display:table;width:50%;margin-left:auto; margin-right:auto; border: 2px solid gray; border-radius: 5px;">
+			<div style="display:table-row; background-color: lightblue;">
+				<div style="display:table-cell; padding-top: 1em; padding-left: 1em; font-size:large; ">
+<?php
+	if(isset($_SESSION['user'])){
+?>
+					<a href="/viewpoll.php?pollid=<?= $row['poll_id']?>"><?= $row['title']?></a>
+<?php
+	}
+	else{
+?>
+					<label><?= $row['title']?></label>
+<?php
+	}
+?>
+				</div>
+				<div style="display:table-cell"></div>
+			</div>
+			<div style="display:table-row">
+				<div style="display:table-cell; padding-left: 1em; padding-top: 1em;width:50%">
+					<label style="border-left: 2px solid lightgray"><?= $row['description']?></label>
+				</div>
+				<div style="display:table-cell;width:50%;">
+					<label>Poll closes at: <?= $row['end_date'] ?></label>
+				</div>
+			</div>
+		</div>
+		<br />
+<?php
+		
+	}
+	echo <<<_END
+		</body></html>
 ?>
